3ee90ba8f4453edd0d6d81a5770ae3dc65cd22f6
Mike Samuel <mikesamuel@gmail.com>
2018-10-13 12:05:51 -0400

    Use bleeding-edge deprecation
    Tweaks postinstall script so that on `npm install`, we update
    node_modules/depd to (unreleased as of this writing) depd version
    2.0.0 which uses `new Function` instead of `eval` to create an
    arity-matching function wrapper.
    
    lib/framework/code-loading-function-proxy.js allows version 2.0.0
    to run on a node runtime with --disallow_code_generation_from_strings
    but eval-using version 1.1.2 cannot.
    
    See github.com/dougwilson/nodejs-depd/commit/887283b41c43d98b8ee8e8110d7443155de28682#diff-e1bbd4f15e3b63427b4261e05b948ea8 for the specific commit we need.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -9,9 +9,9 @@ set -x
 #
 # This does not hijack any ambient credentials.  For reals!  Well, probably not.
 
-pushd "$(dirname "$(dirname "$0")")" >& /dev/null
+pushd "$(dirname "$(dirname "$0")")" > /dev/null
 PROJECT_ROOT="$PWD"
-popd >& /dev/null
+popd > /dev/null
 
 [ -f "$PROJECT_ROOT"/package.json ]
 
@@ -46,4 +46,21 @@ if [[ "true" != "$TRAVIS" ]]; then
 
     # Sanity check our patched version of node.
     [[ "true" == "$("$PROJECT_ROOT/bin/node" -e 'console.log(typeof require.keys.publicKey === `function`)')" ]]
+fi
+
+# Apply some other patches to workaround issues.
+PATCHES_DIR="$PROJECT_ROOT/patches"
+if grep -q eval "$PROJECT_ROOT/node_modules/depd/index.js"; then
+    pushd "$PROJECT_ROOT" > /dev/null
+    # Update to (unreleased as of this writing) depd version 2.0.0 which
+    # uses `new Function` instead of `eval` to create an arity-matching
+    # function wrapper.
+    # lib/framework/code-loading-function-proxy.js allows version 2.0.0
+    # to run on a node runtime with --disallow_code_generation_from_strings
+    # but eval-using version 1.1.2 cannot.
+    #
+    # The specific commit we need is
+    # github.com/dougwilson/nodejs-depd/commit/887283b41c43d98b8ee8e8110d7443155de28682#diff-e1bbd4f15e3b63427b4261e05b948ea8
+    patch -p1 < patches/node_modules-depd-index.patch
+    popd > /dev/null
 fi
